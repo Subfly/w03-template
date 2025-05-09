f29184d8b29c34d71f8918ff014c2f26030aca6b
Subfly <alitahasubfly@gmail.com>
2025-05-09 13:42:03 +0200

    Add tests

--- server/src/main/java/de/tum/aet/devops25/w03/controller/CanteenController.java
+++ server/src/main/java/de/tum/aet/devops25/w03/controller/CanteenController.java
@@ -25,7 +25,12 @@ public class CanteenController {
      */
     @GetMapping("/{canteenName}/today")
     public ResponseEntity<List<Dish>> getTodayMeals(@PathVariable String canteenName) {
-        List<Dish> meals =  canteenService.getTodayMeals(canteenName);
-        return ResponseEntity.ok(meals);
+        List<Dish> todayMeals = canteenService.getTodayMeals(canteenName);
+
+        if (todayMeals.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(todayMeals);
     }
 }
